031fc5c75df709d1285c668aa3989e2b385da274
Tor Steinar <119490595+tor30122@users.noreply.github.com>
2025-04-12 16:21:27 +0200

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module"
+  },
+  "rules": {
+    "no-var": "error",
+    "prefer-const": "error",
+    "prefer-template": "error",
+    "arrow-body-style": ["error", "as-needed"],
+    "prefer-arrow-callback": "error"
+  }
+}
